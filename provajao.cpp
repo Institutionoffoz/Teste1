876c0d4573be0ad2e526a1052dbc375363ca4c87
BrunoAttuy <brunomenattuy@gmail.com>
2021-06-22 10:22:30 -0300

    prova do jao
    u

---
+++ provajao.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+int main() {
+
+    int altura;
+
+    char sexo;
+
+    float pesoIdeal;
+
+    printf("Informe a altura:");
+
+    scanf("%d", &altura);
+
+    printf("Informe o sexo: <M ou F>");
+
+    scanf("%c", &sexo);
+
+    if (sexo == 'm' || sexo == 'M')
+
+        pesoIdeal = (72.7 * altura) - 58;
+
+    else
+
+        pesoIdeal = (62.1 * altura) - 44.7;
+
+    printf("O peso ideal e: %f", pesoIdeal);
+
+ }
